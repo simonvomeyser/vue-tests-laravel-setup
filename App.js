f562f7ea8df5d2b08c1f7a6bc7d58b97ae9c5903
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-11-06 15:11:20 +0100

    Add last stubs and copy them over

--- App.js
+++ App.js
@@ -7,7 +7,16 @@ module.exports = {
     console.log('Env Okay!')
   },
   run: () => {
-    fs.mkdirSync(process.cwd() + '/tests/Vue', {recursive: true})
-    fsExtra.copySync(path.resolve(__dirname, './stubs/Counter.spec.js'), '/tests/ExampleCounter.spec.js');
+    const cwd = process.cwd();
+
+    fsExtra.copySync(cwd  +  '/stubs/.babelrc', cwd + '/.babelrc');
+
+    fs.mkdirSync(cwd + '/tests/Vue', {recursive: true})
+
+    fsExtra.copySync(cwd  +  '/stubs/Counter.spec.js', cwd + '/tests/Vue/ExampleCounter.spec.js');
+    fsExtra.copySync(cwd  +  '/stubs/setup.js', cwd + '/tests/Vue/setup.js');
+
+    fs.mkdirSync(cwd + '/resources/js', {recursive: true})
+    fsExtra.copySync(cwd  +  '/stubs/Counter.vue', cwd + '/resources/js/ExampleCounter.vue');
   }
 }
